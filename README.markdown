8bca6f109a5c985b0d143b85ecf22d9c152e5b6c
Chris <Chris@Chris-MacBook-Pro.local>
2021-12-23 17:13:42 +0800

    20211221

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Animated Christmas Card
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chrislinovo/pen/KKXqLaM](https://codepen.io/chrislinovo/pen/KKXqLaM).
+
+Animated Christmas card
---
+++ src/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  var $clickMe = $('.click-icon'),
+      $card = $('.card');
+
+  $card.on('click', function() {
+ 
+		$(this).toggleClass('is-opened');
+    $clickMe.toggleClass('is-hidden');
+   
+  $("#text").html("Helloworld!"); 
+
+	});
+
+});
+
+ 
